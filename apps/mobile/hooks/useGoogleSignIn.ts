c8a9be8005f7711bdbbe95cb3d8fd6184c04e442
knzn <rdacles08@gmail.com>
2026-04-01 16:57:09 +0800

    fix: add web client ID for Google Sign-In on Android

--- apps/mobile/hooks/useGoogleSignIn.ts
+++ apps/mobile/hooks/useGoogleSignIn.ts
@@ -7,6 +7,7 @@ import { apiRequest } from '../lib/api'
 WebBrowser.maybeCompleteAuthSession()
 
 const ANDROID_CLIENT_ID = process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID ?? ''
+const WEB_CLIENT_ID     = process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID ?? ''
 
 type AuthResponse = {
   accessToken: string
@@ -21,6 +22,7 @@ export function useGoogleSignIn(onSuccess: () => void) {
 
   const [request, response, promptAsync] = Google.useAuthRequest({
     androidClientId: ANDROID_CLIENT_ID,
+    webClientId:     WEB_CLIENT_ID,
   })
 
   useEffect(() => {
